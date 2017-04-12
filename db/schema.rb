ed96e00308f8c8a123786497ec6799771f398934
Yusuke Sangenya <longinus.eva@gmail.com>
2017-04-13 00:42:29 +0900

    install sorcery

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412151326) do
+ActiveRecord::Schema.define(version: 20170412153841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.text     "name"
-    t.text     "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email",            null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
