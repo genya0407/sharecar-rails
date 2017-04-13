0309a2320110b189720247df1560a3a069c1c04d
Yusuke Sangenya <longinus.eva@gmail.com>
2017-04-13 19:50:58 +0900

    car index

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,5 @@
 class CarsController < ApplicationController
   def show
-    @car = Car.find(params[:id])
+    @car = Car.find(params[:id]).join(:user_drives, :drives)
   end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,9 @@
 class Car < ApplicationRecord
   has_many :bookings
-  has_many :drives
+  has_many :drives, class_name: 'Drive'
+
+  def occupied?
+    now = Time.zone.now
+    drives.where("start_at < ? AND ? <= end_at", now, now).exists?
+  end
 end
