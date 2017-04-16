86c16ce58551ea0ed9bdd1be02641f5c274fc80f
Yusuke Sangenya <longinus.eva@gmail.com>
2017-04-17 06:49:41 +0900

    driveを終了できるように

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,4 +5,12 @@ class Car < ApplicationRecord
   def occupied?
     drives.where(end_meter: nil).exists?
   end
+
+  def using_drive(user_id)
+    @using_drive ||= drives.find_by(end_meter: nil, user_id: user_id)
+  end
+
+  def using?(user_id)
+    using_drive(user_id).present?
+  end
 end
