17044d43080cee2dae5babcb33ed1f606f241d8a
Yusuke Sangenya <longinus.eva@gmail.com>
2017-04-17 08:26:21 +0900

    secret

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # email
-  config.action_mailer.default_url_options = { :host => 'kuminecraft.xyz', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.site_domain , :protocol => 'https' }
 end
