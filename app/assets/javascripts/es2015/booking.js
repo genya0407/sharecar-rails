32d1c5b5a26ae086fc45e4334c7d99aa3bf34088
Yusuke Sangenya <longinus.eva@gmail.com>
2017-04-24 11:28:57 +0900

    datetimepickerの見た目だけ作った

---
+++ app/assets/javascripts/es2015/booking.js
@@ -0,0 +1,31 @@
+/*
+import MaterialDateTimePicker from 'material-datetime-picker';
+
+const inputs = document.querySelectorAll('.datetimepicker');
+
+inputs.forEach((input) => {
+	const picker = new MaterialDateTimePicker()
+	    .on('submit', (val) => {
+	      input.value = val.format("DD/MM/YYYY");
+	    });
+
+	input.addEventListener('focus', () => picker.open());
+});
+*/
+
+$('.datepicker').pickadate({
+	selectMonths: true, // Creates a dropdown to control month
+	selectYears: 15, // Creates a dropdown of 15 years to control year
+    monthsFull: [ '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' ],
+    monthsShort: [ '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' ],
+    weekdaysFull: [ '日曜' , '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
+    weekdaysShort: [ '日曜' ,  '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
+    today: '今日',
+    close: 'OK',
+    clear: 'キャンセル',
+    format: 'yyyy年mm月dd日'
+});
+
+$(document).ready(function() {
+	$('select').material_select();
+});
