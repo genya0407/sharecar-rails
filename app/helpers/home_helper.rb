24c31e1158ed00dfdfeabf2c4d94df3d846b0a31
Yusuke Sangenya <longinus.eva@gmail.com>
2017-04-27 20:17:52 +0900

    trivial appearance modification

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
   def simple_datetime(datetime)
     if Time.zone.now.to_date == datetime.to_date
-      datetime.in_time_zone.to_s :time_only
+      '本日 ' + (datetime.in_time_zone.to_s :time_only)
     else
       datetime.in_time_zone.to_s :ja
     end
