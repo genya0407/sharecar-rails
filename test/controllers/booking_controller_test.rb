5c47f06343574cb3b21356d9b7354d3b7275506c
Yusuke Sangenya <longinus.eva@gmail.com>
2017-04-28 00:50:47 +0900

    不正な予約をするときにエラーを出すというテストを、予約とコンフリクトするすべてのパターンについて行なうように

--- test/controllers/booking_controller_test.rb
+++ test/controllers/booking_controller_test.rb
@@ -37,19 +37,19 @@ class BookingControllerTest < BaseControllerTest
 
   test '#create 重複する予約がある時、予約は作成されず、エラーを表示すること' do
     booking = build(:booking)
-    create_booking_end_in_range(booking)
 
-    assert_difference "Booking.where(car_id: #{@car.id}).count", 0 do
-      post car_bookings_path(car_id: booking.car.id), params: {
-        booking_form_create: {
-          start_at_date: booking.start_at.to_date,
-          start_at_hour: booking.start_at.hour,
-          end_at_date: booking.end_at.to_date,
-          end_at_hour: booking.end_at.hour
+    with_conflicted_bookings booking do
+      assert_difference "Booking.where(car_id: #{@car.id}).count", 0 do
+        post car_bookings_path(car_id: booking.car.id), params: {
+          booking_form_create: {
+            start_at_date: booking.start_at.to_date,
+            start_at_hour: booking.start_at.hour,
+            end_at_date: booking.end_at.to_date,
+            end_at_hour: booking.end_at.hour
+          }
         }
-      }
+      end
+      assert_select '#errors'
     end
-
-    assert_select '#errors'
   end
 end
