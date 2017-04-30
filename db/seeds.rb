8e2fe2ea0c32d37130138e5216e470d504b38dd4
Yusuke Sangenya <longinus.eva@gmail.com>
2017-04-30 18:16:00 +0900

    seedでdriveをたくさん生成するように

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.new(
+user = User.new(
   name: 'Test User',
   email: 'example@example.com',
   password: 'password',
   password_confirmation: 'password'
-).save!
+)
+user.save!
 
 Car.create!((1..5).map { { name: Faker::Cat.name } })
+car = Car.first
+
+start_meter = 0
+end_meter = rand(10..100)
+start_at = Time.zone.now
+end_at = start_at + rand(2..10).hours
+drive_params = 100.times.map do
+  params = {
+    user: user,
+    car: car,
+    start_meter: start_meter,
+    end_meter: end_meter,
+    start_at: start_at,
+    end_at: end_at
+  }
+  start_meter = end_meter
+  end_meter = start_meter + rand(10..100)
+  start_at = end_at + rand(5..20).hours
+  end_at = start_at + rand(2..10).hours
+  params
+end
+Drive.create(drive_params)
