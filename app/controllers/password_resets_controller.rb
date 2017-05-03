fbf5c49608d52ffc88c2da7bc0eb91c7ec67775d
Yusuke Sangenya <longinus.eva@gmail.com>
2017-05-03 17:55:22 +0900

    reset password mail が届くように

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_email(params[:email])
     @user.deliver_reset_password_instructions! if @user
 
-    redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
+    redirect_to(root_path, :notice => 'メールが送信されました。')
   end
     
   def edit
--- config/environment.rb
+++ config/environment.rb
@@ -11,5 +11,6 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
-  :enable_starttls_auto => true
+  :enable_starttls_auto => true,
+  :tls => true
 }
