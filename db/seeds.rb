31ceebe0ba0e17b779ab5de6cb5c7a2585ee9c32
Yusuke Sangenya <longinus.eva@gmail.com>
2017-07-04 22:34:11 +0900

    seedデータに複数人のユーザーが存在するように

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,17 @@ user.save!
 user.activate!
 user.admin!
 
+3.times.each do
+  User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password',
+    phone_number: Faker::PhoneNumber.phone_number
+  ).save!
+end
+users = User.all
+
 Car.create!((1..5).map { { name: Faker::Cat.name } })
 car = Car.first
 
@@ -24,9 +35,9 @@ start_meter = 0
 end_meter = rand(10..100)
 start_at = Time.zone.now
 end_at = start_at + rand(2..10).hours
-drive_params = 100.times.map do
+drive_params = 30.times.map do
   params = {
-    user: user,
+    user: users.sample,
     car: car,
     start_meter: start_meter,
     end_meter: end_meter,
