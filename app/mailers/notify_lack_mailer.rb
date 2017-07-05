3f12a10e10843db471b0ab19565e3fb7ee68f787
Yusuke Sangenya <longinus.eva@gmail.com>
2017-07-05 21:06:01 +0900

    件名が変化するように

--- app/mailers/notify_lack_mailer.rb
+++ app/mailers/notify_lack_mailer.rb
@@ -1,7 +1,12 @@
 class NotifyLackMailer < ApplicationMailer
   def lack_exist_email(user, cars)
     @cars = cars
+    subject = if cars.empty?
+                '乗車記録に欠落はありません'
+              else
+                '乗車記録に欠落があります'
+              end
     mail(:to => user.email,
-         :subject => "乗車記録に欠落があります")
+         :subject => subject)
   end
 end
