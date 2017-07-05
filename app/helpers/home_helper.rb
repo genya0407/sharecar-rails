b15be1171f4a038863751b17b737323f4b25685c
Yusuke Sangenya <longinus.eva@gmail.com>
2017-07-05 18:46:40 +0900

    driveやbookingのstart_atやend_atがnilのときも動くように

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,7 @@
 module HomeHelper
   def simple_datetime(datetime)
+    return '???' if datetime.nil?
+
     if Time.zone.now.to_date == datetime.to_date
       '本日 ' + (datetime.in_time_zone.to_s :time_only)
     else
