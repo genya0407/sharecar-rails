c384fada76b81d27fe8fd311ba5c8a5384c82687
Yusuke Sangenya <longinus.eva@gmail.com>
2017-07-05 19:16:21 +0900

    管理者権限でdriveを作成したり削除したり編集できるように

--- app/controllers/admin/drives_controller.rb
+++ app/controllers/admin/drives_controller.rb
@@ -19,11 +19,58 @@ class Admin::DrivesController < ApplicationController
   end
 
   def new
+    @form = DriveForm::Admin.new(new_params)
   end
 
   def create
+    @form = DriveForm::Admin.new(create_params)
+
+    if @form.valid?
+      @form.save!
+
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @drive = Drive.find(params[:id])
+    @form = DriveForm::Admin.from_drive(@drive)
   end
 
   def update
+    @form = DriveForm::Admin.new(create_params)
+
+    if @form.valid?
+      drive = Car.find(params[:car_id]).drives.find(params[:id])
+      drive.attributes = @form.attributes
+      drive.save!
+
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Drive.find(params[:id]).destroy
+
+    redirect_to action: :index
+  end
+
+  private
+  def new_params
+    {
+      start_meter: params[:start_meter],
+      end_meter: params[:end_meter],
+      car_id: params[:car_id]
+    }
+  end
+
+  def create_params
+    params.require(:drive_form_admin)
+          .permit(:user_id, :start_meter, :end_meter).to_h
+          .merge(car_id: params[:car_id])
   end
 end
--- test/controllers/admin/drives_controller_test.rb
+++ test/controllers/admin/drives_controller_test.rb
@@ -30,4 +30,42 @@ class Admin::DrivesControllerTest < BaseControllerTest
     assert_select '.drive', all_drives_count
     assert_select '.drive-form-create-admin', lacking_drive_count
   end
+
+  test 'create 乗車記録を作成できること' do
+    car = create(:car)
+    drive_params = build(:drive, car: car).attributes.slice('user_id', 'start_meter', 'end_meter')
+
+    assert_difference 'Drive.count', +1 do
+      post admin_car_drives_path(car_id: car.id), params: { drive_form_admin: drive_params }
+    end
+  end
+
+  test 'update 乗車記録を編集できること' do
+    drive = create(:drive)
+    new_start_meter = rand((drive.start_meter)...(drive.end_meter))
+    new_end_meter = rand((new_start_meter)...(drive.end_meter))
+    new_user_id = create(:user).id
+ 
+    put admin_car_drive_path(car_id: drive.car.id, id: drive.id),
+        params: {
+          drive_form_admin: {
+            start_meter: new_start_meter,
+            end_meter: new_end_meter,
+            user_id: new_user_id
+          }
+        }
+    drive.reload
+    assert_equal new_start_meter, drive.start_meter
+    assert_equal new_end_meter, drive.end_meter
+    assert_equal new_user_id, drive.user_id
+  end
+
+  test 'destroy 乗車記録を削除できること' do
+    car = create(:car)
+    create_continuous_drives(car, n: 10)
+
+    assert_difference 'Drive.count', -1 do
+      delete admin_car_drive_path(car_id: car.id, id: car.drives.all.sample.id)
+    end
+  end
 end
