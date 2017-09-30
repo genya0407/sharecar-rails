7d557e9d6c652a71fb0e9603e775400d414b2aff
Yusuke Sangenya <longinus.eva@gmail.com>
2017-09-30 14:14:44 +0900

    create payment

---
+++ db/migrate/20170930050940_create_payments.rb
@@ -0,0 +1,10 @@
+class CreatePayments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :payments do |t|
+      t.references :users, null: false
+      t.integer :amount, null: false
+
+      t.timestamps
+    end
+  end
+end
--- test/factories/term.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :term do
-    name { Faker::Name.name }
-  end
-end
