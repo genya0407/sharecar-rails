eed423f10b3564a4b1e209838fc7aa8b5185a7c4
Yusuke Sangenya <longinus.eva@gmail.com>
2018-04-01 22:51:47 +0900

    Remove needless file.

--- hoge.rb
+++
@@ -1,8 +0,0 @@
-Car.all.each do |car|
-  Consumption.create(
-    car: car,
-    start_at: Time.zone.local(2017, 10, 1, 0, 0, 0),
-    end_at: Time.zone.local(2017, 3, 31, 23, 59, 59),
-    price: 0
-  )
-end
