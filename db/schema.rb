381d103be98fa48710c47cee8aa6061d0c772a37
Yusuke Sangenya <longinus.eva@gmail.com>
2018-05-26 11:12:59 +0900

    Create note table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,93 +10,101 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327105932) do
+ActiveRecord::Schema.define(version: 2018_05_26_021019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.datetime "start_at",   null: false
-    t.datetime "end_at",     null: false
+    t.datetime "start_at", null: false
+    t.datetime "end_at", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "car_id",     null: false
-    t.integer  "user_id",    null: false
-    t.index ["car_id"], name: "index_bookings_on_car_id", using: :btree
-    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+    t.integer "car_id", null: false
+    t.integer "user_id", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "cars", force: :cascade do |t|
-    t.string   "name",                   null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "status",     default: 0, null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
   end
 
   create_table "consumptions", force: :cascade do |t|
-    t.integer  "car_id"
-    t.float    "price"
+    t.integer "car_id"
+    t.float "price"
     t.datetime "start_at"
     t.datetime "end_at"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "finished",   default: false, null: false
-    t.index ["car_id"], name: "index_consumptions_on_car_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "finished", default: false, null: false
+    t.index ["car_id"], name: "index_consumptions_on_car_id"
   end
 
   create_table "drives", force: :cascade do |t|
     t.datetime "start_at"
     t.datetime "end_at"
-    t.integer  "start_meter", null: false
-    t.integer  "end_meter"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "car_id",      null: false
-    t.integer  "user_id",     null: false
-    t.index ["car_id"], name: "index_drives_on_car_id", using: :btree
-    t.index ["user_id"], name: "index_drives_on_user_id", using: :btree
+    t.integer "start_meter", null: false
+    t.integer "end_meter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "car_id", null: false
+    t.integer "user_id", null: false
+    t.index ["car_id"], name: "index_drives_on_car_id"
+    t.index ["user_id"], name: "index_drives_on_user_id"
   end
 
   create_table "fuels", force: :cascade do |t|
-    t.integer  "user_id",                    null: false
-    t.integer  "car_id",                     null: false
-    t.integer  "amount",                     null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "checked",    default: false, null: false
-    t.index ["car_id"], name: "index_fuels_on_car_id", using: :btree
-    t.index ["user_id"], name: "index_fuels_on_user_id", using: :btree
+    t.integer "user_id", null: false
+    t.integer "car_id", null: false
+    t.integer "amount", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "checked", default: false, null: false
+    t.index ["car_id"], name: "index_fuels_on_car_id"
+    t.index ["user_id"], name: "index_fuels_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.bigint "car_id"
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_notes_on_car_id"
   end
 
   create_table "payments", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "amount",     null: false
+    t.integer "user_id", null: false
+    t.integer "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_payments_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email",                                       null: false
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.string   "crypted_password"
-    t.string   "salt"
-    t.string   "reset_password_token"
+    t.string "name"
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
-    t.integer  "permission",                      default: 5, null: false
-    t.string   "activation_state"
-    t.string   "activation_token"
+    t.integer "permission", default: 5, null: false
+    t.string "activation_state"
+    t.string "activation_token"
     t.datetime "activation_token_expires_at"
-    t.string   "phone_number"
-    t.string   "remember_me_token"
+    t.string "phone_number"
+    t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.index ["activation_token"], name: "index_users_on_activation_token", using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
+    t.index ["activation_token"], name: "index_users_on_activation_token"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
