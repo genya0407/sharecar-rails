640b3075e1a8d2e31160852f2ddb7a9d47109469
Yusuke Sangenya <longinus.eva@gmail.com>
2018-08-01 21:42:49 +0900

    Consumption calculate feature.

--- app/models/consumption.rb
+++ app/models/consumption.rb
@@ -5,14 +5,18 @@ class Consumption < ApplicationRecord
     where(finished: false)
   end
 
-  def self.calc_consumption(target_fuels, target_drives)
-    target_fuels.sum(&:amount) / target_drives.sum(&:distance).to_f
-  end
-
   def calc_fee_of(user)
     drives = target_drives_of(user)
     total_distance = drives.sum(&:distance)
     total_distance * price
+  end
+
+  def recalculate
+    target_fuels = car.fuels.in(start_at, end_at)
+    target_drives = car.drives.in(start_at, end_at)
+
+    new_price = target_fuels.sum(&:amount) / target_drives.sum(&:distance).to_f
+    self.update(price: new_price)
   end
 
   private
