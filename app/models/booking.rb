d770db12f440a465fbb61be68a7af46e5619c98a
Yusuke Sangenya <longinus.eva@gmail.com>
2020-12-28 20:48:14 +0900

    各validationが独立して動くように

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,12 +6,9 @@ class Booking < ApplicationRecord
   validates :end_at, presence: true
   validates :car_id, presence: true
   validates :user_id, presence: true
-
-  with_options if: -> { car.present? && start_at.present? && end_at.present? } do |basically_valid|
-    basically_valid.validate :end_should_be_greater_than_start
-    basically_valid.validate :conflicted_bookings_should_not_exist
-    basically_valid.validate :conflicted_drives_should_not_exist
-  end
+  validate :end_should_be_greater_than_start
+  validate :conflicted_bookings_should_not_exist
+  validate :conflicted_drives_should_not_exist
 
   def self.in_effect
     where('end_at > ?', Time.zone.now).order('start_at')
@@ -24,14 +21,20 @@ class Booking < ApplicationRecord
   private
 
   def end_should_be_greater_than_start
+    return unless car.present? && start_at.present? && end_at.present?
+
     errors.add(:end_at, '終了時刻は開始時刻より後に設定してください') unless start_at < end_at
   end
 
   def conflicted_bookings_should_not_exist
+    return unless car.present? && start_at.present? && end_at.present?
+
     errors.add(:start_at, "その期間は#{conflicted_bookings.first.user.name}が車を予約しています") if conflicted_bookings.exists?
   end
 
   def conflicted_drives_should_not_exist
+    return unless car.present? && start_at.present? && end_at.present?
+
     errors.add(:start_at, "その期間は#{conflicted_drives.first.user.name}が車を使用しています") if conflicted_drives.exists?
   end
 
