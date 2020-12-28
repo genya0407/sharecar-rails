12cd79fd0da67a8e2c8c1649adacee3f6f12cf8a
Yusuke Sangenya <longinus.eva@gmail.com>
2020-12-28 20:11:13 +0900

    dont use helper

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -8,10 +8,10 @@ class BookingTest < ActiveSupport::TestCase
 
   test 'car.bookings.in_effect 完遂されていないかもしれない予約を取得する' do
     car = create(:car)
+    bookings_in_effect = Array.new(2) { |i| create(:booking, car: car, start_at: (i * 2).days.since, end_at: (i * 2 + 1).days.since) }
+    _bookings_not_in_effect = create(:booking, car: car, start_at: 2.days.ago, end_at: 1.day.ago)
 
-    bookings_count_in_effect = create_in_effect(car).count
-
-    assert_equal car.bookings.in_effect.count, bookings_count_in_effect
+    assert_equal car.bookings.in_effect.count, bookings_in_effect.size
   end
 
   test 'Booking#conflicted_bookings 重複する予約を取得する' do
