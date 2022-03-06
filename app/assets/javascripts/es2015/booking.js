71fde7da582ef151877e1c635f8fbbbe8d441b26
Yusuke Sangenya <longinus.eva@gmail.com>
2022-03-07 00:51:08 +0900

    datepicker

--- app/assets/javascripts/es2015/booking.js
+++ app/assets/javascripts/es2015/booking.js
@@ -12,16 +12,3 @@ inputs.forEach((input) => {
 	input.addEventListener('focus', () => picker.open());
 });
 */
-
-$('.datepicker').pickadate({
-	selectMonths: true, // Creates a dropdown to control month
-	selectYears: 15, // Creates a dropdown of 15 years to control year
-    monthsFull: [ '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' ],
-    monthsShort: [ '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' ],
-    weekdaysFull: [ '日曜' , '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
-    weekdaysShort: [ '日曜' ,  '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
-    today: '今日',
-    close: 'OK',
-    clear: 'キャンセル',
-    format: 'yyyy-mm-dd'
-});
