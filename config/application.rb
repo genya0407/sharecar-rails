dd61832130057ae2b52de1c674bc26a51b85442a
Yusuke Sangenya <longinus.eva@gmail.com>
2022-03-11 13:58:38 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,18 @@
 require_relative 'boot'
 
 require 'rails'
-
-%w[
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  rails/test_unit/railtie
-  sprockets/railtie
-].each do |railtie|
-  require railtie
-end
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+# require "active_storage/engine"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require 'action_view/railtie'
+# require "action_cable/engine"
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,13 +20,11 @@ Bundler.require(*Rails.groups)
 
 module SharecarRails
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
 
     config.time_zone = 'Tokyo'
     config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
     config.autoload_paths += ["#{Rails.root}/app/forms"]
-    config.active_record.legacy_connection_handling = false
   end
 end
